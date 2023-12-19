da08c733b49fa13f336827bcb4fbb01b54fc6db9
Eduardo Hernandez <eduardo@chatbot-plus.com>
2023-12-19 23:12:27 +0100

    add new controller versions

--- src/main/java/com/main/logicgate/controller/ArticleController.java
+++ src/main/java/com/main/logicgate/controller/ArticleController.java
@@ -5,28 +5,21 @@ import com.main.logicgate.dto.NewArticleRequestDTO;
 import com.main.logicgate.model.ArticleModel;
 import com.main.logicgate.model.UserModel;
 import com.main.logicgate.repository.ArticleRepository;
-
 import com.main.logicgate.repository.UserRepository;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootApplication
 @RestController
 @RequestMapping("api/v1/articles")
+@RequiredArgsConstructor
 @CrossOrigin
 public class ArticleController {
     private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
-
-    public ArticleController(ArticleRepository articleRepository, UserRepository userRepository) {
-        this.articleRepository = articleRepository;
-        this.userRepository = userRepository;
-    }
 
     @GetMapping
     public List<ArticleModel> getArticles() {
@@ -56,8 +49,8 @@ public class ArticleController {
             newArticle.setPhoto(request.getPhoto());
             newArticle.setProgrammingLanguage(request.getProgrammingLanguage());
             newArticle.setAuthor(request.getAuthor());
-            articleRepository.save(newArticle);
 
+            articleRepository.save(newArticle);
             return ResponseEntity.status(HttpStatus.CREATED).body("Article created successfully");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating article");
