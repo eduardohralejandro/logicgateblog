238593545c6f59758524b7e391480f26190e1247
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-25 14:59:18 +0100

    add new corsconfiguration source

--- src/main/java/com/main/logicgate/Application.java
+++ src/main/java/com/main/logicgate/Application.java
@@ -25,37 +25,37 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		config.setAllowCredentials(true);
-		config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
-		config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-		config.setExposedHeaders(Arrays.asList("x-auth-token"));
-
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		config.setAllowedOrigins(Collections.singletonList("*"));
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
 //		config.addAllowedOrigin("*");
 //		config.addAllowedHeader("*");
-//		config.addAllowedMethod("GET");
-//		config.addAllowedMethod("POST");
-//		config.addAllowedMethod("PUT");
-//		config.addAllowedMethod("DELETE");
 //		config.addAllowedMethod("OPTIONS");
-		config.setAllowedOrigins(Arrays.asList("https://logicgatesblog-9da8a03a3c58.herokuapp.com/"));
-		source.registerCorsConfiguration("/**", config);
-
-
-		return new CorsFilter(source);
-	}
+//		config.addAllowedMethod("HEAD");
+//		config.addAllowedMethod("GET");
+//		config.addAllowedMethod("PUT");
+//		config.addAllowedMethod("POST");
+//		config.addAllowedMethod("DELETE");
+//		config.addAllowedMethod("PATCH");
+//		config.setAllowCredentials(true);
+//		config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
+//		config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+//		config.setExposedHeaders(Arrays.asList("x-auth-token"));
+//
+//		config.setAllowedHeaders(Collections.singletonList("*"));
+//		config.setAllowedOrigins(Collections.singletonList("*"));
+////		config.addAllowedOrigin("*");
+////		config.addAllowedHeader("*");
+////		config.addAllowedMethod("GET");
+////		config.addAllowedMethod("POST");
+////		config.addAllowedMethod("PUT");
+////		config.addAllowedMethod("DELETE");
+////		config.addAllowedMethod("OPTIONS");
+//		config.setAllowedOrigins(Arrays.asList("https://logicgatesblog-9da8a03a3c58.herokuapp.com/"));
+//		source.registerCorsConfiguration("/**", config);
+//
+//
+//		return new CorsFilter(source);
+//	}
 }
--- src/main/java/com/main/logicgate/security/SecurityConfiguration.java
+++ src/main/java/com/main/logicgate/security/SecurityConfiguration.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -29,7 +36,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.cors(AbstractHttpConfigurer::disable)
+        http.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers(WHITE_LIST_URLS)
@@ -43,4 +50,14 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://logicgatesblog-9da8a03a3c58.herokuapp.com/"));
+        configuration.setAllowedMethods(Arrays.asList("GET"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
