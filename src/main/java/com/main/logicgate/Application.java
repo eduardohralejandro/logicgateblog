883aa21e7853d7ced4672fcda00ae3bcb3dba6b6
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-25 09:34:04 +0100

    new cors filter

--- src/main/java/com/main/logicgate/Application.java
+++ src/main/java/com/main/logicgate/Application.java
@@ -25,21 +25,30 @@ public class Application {
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
-		config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-		config.setExposedHeaders(Arrays.asList("x-auth-token"));
-
-//		config.setAllowedHeaders(Collections.singletonList("*"));
-//		config.setAllowedOrigins(Collections.singletonList("*"));
 		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
 		config.addAllowedMethod("OPTIONS");
-		config.setAllowedOrigins(Arrays.asList("https://logicgatesblog-9da8a03a3c58.herokuapp.com/"));
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+//		config.setAllowCredentials(true);
+//		config.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
+//		config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+//		config.setExposedHeaders(Arrays.asList("x-auth-token"));
+//
+////		config.setAllowedHeaders(Collections.singletonList("*"));
+////		config.setAllowedOrigins(Collections.singletonList("*"));
+//		config.addAllowedOrigin("*");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("GET");
+//		config.addAllowedMethod("POST");
+//		config.addAllowedMethod("PUT");
+//		config.addAllowedMethod("DELETE");
+//		config.addAllowedMethod("OPTIONS");
+//		config.setAllowedOrigins(Arrays.asList("https://logicgatesblog-9da8a03a3c58.herokuapp.com/"));
 		source.registerCorsConfiguration("/**", config);
 
 
